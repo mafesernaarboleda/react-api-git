c6f9ee0b9d02e2b4ef9924e255509adb6e37e20c
mafesernaarboleda <maria@batteries911.com>
2017-11-03 07:41:10 -0500

    fixed deploy

--- src/api/profile.js
+++ src/api/profile.js
@@ -2,14 +2,11 @@
  * @author Maria Fernanda Serna
  */
 
-import * as consts from './constants';
-
 const getUser = userName => {
   return fetch(`https://api.github.com/users/${userName}`, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
-      Authorization: `token ${consts.TOKEN_GITHUB}`,
     },
   });
 };
--- src/api/repository.js
+++ src/api/repository.js
@@ -1,14 +1,12 @@
 /**
  * @author Maria Fernanda Serna
  */
-import * as consts from './constants';
 
 const getListReposByUser = userName => {
   return fetch(`https://api.github.com/users/${userName}/repos`, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
-      Authorization: `token ${consts.TOKEN_GITHUB}`,
     },
   });
 };
